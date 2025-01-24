6f29f57d1bfe5d5e2c11b4e5f4ff9d6bcb64d637
NoahChen <noah.chen@batechworks.com>
2025-01-24 15:51:34 +0800

    1343. sliding window

---
+++ src/main/java/com/huhu/algorithm/learn/n1343/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n1343;
+
+/**
+ * <a href="https://leetcode.cn/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/description/">
+ * number of subarrays of size k and average greater than or equal to threshold
+ * </a>
+ */
+interface Solution {
+
+    int numOfSubarrays(int[] arr, int k, int threshold);
+
+}
---
+++ src/test/java/com/huhu/algorithm/learn/n1343/SolutionATest.java
@@ -0,0 +1,10 @@
+package com.huhu.algorithm.learn.n1343;
+
+class SolutionATest extends SolutionTest {
+
+    @Override
+    Solution getSolution() {
+        return new SolutionA();
+    }
+
+}
