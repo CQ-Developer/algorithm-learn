3da294522578672b437e3ccdbf57264e34357b02
NoahChen <noah.chen@batechworks.com>
2025-01-24 11:10:07 +0800

    setup junit test

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,16 +17,28 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
+    testLogging {
+        showStandardStreams = true
+        showStackTraces = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        displayGranularity = 1
+    }
 }
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
     reports {
         html.required = true
+        xml.required = true
+        csv.required = true
     }
 }
+
+group = "com.huhu.algorithm"
+version = "0.0.1"
