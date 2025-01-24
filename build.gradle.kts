3a2f35102fa41119eea0ba60abaebce209386d37
NoahChen <noah.chen@batechworks.com>
2025-01-24 10:22:47 +0800

    initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    jacoco
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        html.required = true
+    }
+}
