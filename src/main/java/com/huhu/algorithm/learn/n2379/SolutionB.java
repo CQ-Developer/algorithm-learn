71e4a5968497085f4ff6a3e2ca21485a69d7eb04
chen <18551681083@163.com>
2025-01-25 17:50:29 +0800

    2379. sliding window

---
+++ src/main/java/com/huhu/algorithm/learn/n2379/SolutionB.java
@@ -0,0 +1,27 @@
+package com.huhu.algorithm.learn.n2379;
+
+/**
+ * sliding window
+ */
+class SolutionB implements Solution {
+
+    @Override
+    public int minimumRecolors(String blocks, int k) {
+        char[] chars = blocks.toCharArray();
+        int n = chars.length, res = k, w = 0;
+        for (int i = 0; i < n; i++) {
+            if (chars[i] == 'W') {
+                w++;
+            }
+            if (i < k - 1) {
+                continue;
+            }
+            res = Math.min(res, w);
+            if (chars[i - k + 1] == 'W') {
+                w--;
+            }
+        }
+        return res;
+    }
+
+}
