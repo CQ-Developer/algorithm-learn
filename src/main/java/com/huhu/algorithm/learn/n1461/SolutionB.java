e23c482f32636a0a205134fc79d998247e90ec7b
NoahChen <noah.chen@batechworks.com>
2025-01-26 17:04:54 +0800

    1461. enumeration

---
+++ src/main/java/com/huhu/algorithm/learn/n1461/SolutionB.java
@@ -0,0 +1,20 @@
+package com.huhu.algorithm.learn.n1461;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * enumeration
+ */
+class SolutionB implements Solution {
+
+    @Override
+    public boolean hasAllCodes(String s, int k) {
+        Set<String> set = new HashSet<>();
+        for (int i = s.length() - k; i >= 0; i--) {
+            set.add(s.substring(i, i + k));
+        }
+        return set.size() == 1 << k;
+    }
+
+}
---
+++ src/test/java/com/huhu/algorithm/learn/n1461/SolutionBTest.java
@@ -0,0 +1,10 @@
+package com.huhu.algorithm.learn.n1461;
+
+class SolutionBTest extends SolutionTest {
+
+    @Override
+    Solution getSolution() {
+        return new SolutionB();
+    }
+
+}
