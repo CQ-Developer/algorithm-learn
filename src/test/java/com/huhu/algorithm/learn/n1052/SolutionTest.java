b1533d87d87fd784f5734753c5f45d741f2784dc
NoahChen <noah.chen@batechworks.com>
2025-01-26 10:01:03 +0800

    1052. sliding window

---
+++ src/test/java/com/huhu/algorithm/learn/n1052/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.huhu.algorithm.learn.n1052;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+abstract class SolutionTest {
+
+    Solution solution;
+
+    abstract Solution getSolution();
+
+    @BeforeEach
+    void setup() {
+        solution = getSolution();
+    }
+
+    @Test
+    void testA() {
+        int[] customers = {1, 0, 1, 2, 1, 1, 7, 5}, grumpy = {0, 1, 0, 1, 0, 1, 0, 1};
+        assertEquals(16, solution.maxSatisfied(customers, grumpy, 3));
+    }
+
+    @Test
+    void testB() {
+        int[] customers = {1}, grumpy = {0};
+        assertEquals(1, solution.maxSatisfied(customers, grumpy, 1));
+    }
+
+}
