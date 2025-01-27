c5177392f26fa43e42c281b5c28c33e547bbfcda
NoahChen <noah.chen@batechworks.com>
2025-01-27 17:26:35 +0800

    1297. maximum number of occurrences of a substring

---
+++ src/main/java/com/huhu/algorithm/learn/n1297/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n1297;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-number-of-occurrences-of-a-substring/description/">
+ * maximum number of occurrences of a substring
+ * </a>
+ */
+interface Solution {
+
+    int maxFreq(String s, int maxLetters, int minSize, int maxSize);
+
+}
