a715d9871e98e4cb388f34094238ab1f145635c8
chen <18551681083@163.com>
2025-02-02 11:43:29 +0800

    1100. find k length substrings with no repeated characters

---
+++ src/main/java/com/huhu/algorithm/learn/n1100/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n1100;
+
+/**
+ * <a href="https://leetcode.cn/problems/find-k-length-substrings-with-no-repeated-characters/description/">
+ * find k length substrings with no repeated characters
+ * </a>
+ */
+interface Solution {
+
+    int numKLenSubstrNoRepeats(String s, int k);
+
+}
