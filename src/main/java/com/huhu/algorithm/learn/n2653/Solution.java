4f76971eb87efdc3b8108115b92d6240d4c1b135
NoahChen <noah.chen@batechworks.com>
2025-02-05 15:36:30 +0800

    2653. sliding subarray beauty

---
+++ src/main/java/com/huhu/algorithm/learn/n2653/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n2653;
+
+/**
+ * <a href="https://leetcode.cn/problems/sliding-subarray-beauty/description/">
+ * sliding subarray beauty
+ * </a>
+ */
+interface Solution {
+
+    int[] getSubarrayBeauty(int[] nums, int k, int x);
+
+}
