fd4970b1c965333f8b55d1a45ca5a1b5b9d7dbd5
NoahChen <noah.chen@batechworks.com>
2025-02-06 12:45:13 +0800

    438. find all anagrams in a string

---
+++ src/test/java/com/huhu/algorithm/learn/n438/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.huhu.algorithm.learn.n438;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+abstract class SolutionTest {
+
+    Solution solution;
+
+    abstract Solution getSolution();
+
+    @BeforeEach
+    void setup() {
+        solution = getSolution();
+    }
+
+    @Test
+    void testA() {
+        assertIterableEquals(List.of(0, 6), solution.findAnagrams("cbaebabacd", "abc"));
+    }
+
+    @Test
+    void testB() {
+        assertIterableEquals(List.of(0, 1, 2), solution.findAnagrams("abab", "ab"));
+    }
+
+}
