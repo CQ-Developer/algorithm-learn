1aa7ef618520d625f0a5af65d3884cc85222cd9d
NoahChen <noah.chen@batechworks.com>
2025-02-10 14:22:19 +0800

    1208. get equal substring within budget

---
+++ src/main/java/com/huhu/algorithm/learn/n1208/Boo.java
@@ -0,0 +1,30 @@
+package com.huhu.algorithm.learn.n1208;
+
+/**
+ * <h1>sliding window</h1>
+ */
+class Boo implements Solution {
+
+    /**
+     * <p>和{@link Aoo}的算法相同, 只是提前计算了每个位置的cost.
+     * 防止重复计算.</p>
+     */
+    @Override
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int[] costs = new int[n];
+        for (int i = 0; i < n; i++) {
+            costs[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+        int mx = 0;
+        for (int sum = 0, l = 0, r = 0; r < n; r++) {
+            sum += costs[r];
+            for (; sum > maxCost; l++) {
+                sum -= costs[l];
+            }
+            mx = Math.max(mx, r - l + 1);
+        }
+        return mx;
+    }
+
+}
