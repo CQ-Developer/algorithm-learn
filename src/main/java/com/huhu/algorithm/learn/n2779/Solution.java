a622d89aebed846b5ef26c2fe09cf98e08b96d59
NoahChen <noah.chen@batechworks.com>
2025-02-11 13:52:43 +0800

    2779. maximum beauty of an array after applying operation

---
+++ src/main/java/com/huhu/algorithm/learn/n2779/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n2779;
+
+/**
+ * <a href="https://leetcode.cn/problems/maximum-beauty-of-an-array-after-applying-operation/description/">
+ * maximum beauty of an array after applying operation
+ * </a>
+ */
+interface Solution {
+
+    int maximumBeauty(int[] nums, int k);
+
+}
