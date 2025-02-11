5571f9eed65a1370493845132758b83041f035d2
NoahChen <noah.chen@batechworks.com>
2025-02-11 17:06:21 +0800

    1004. max consecutive ones iii

---
+++ src/main/java/com/huhu/algorithm/learn/n1004/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n1004;
+
+/**
+ * <a href="https://leetcode.cn/problems/max-consecutive-ones-iii/description/">
+ * max consecutive ones iii
+ * </a>
+ */
+interface Solution {
+
+    int longestOnes(int[] nums, int k);
+
+}
---
+++ src/test/java/com/huhu/algorithm/learn/n1004/AooTest.java
@@ -0,0 +1,10 @@
+package com.huhu.algorithm.learn.n1004;
+
+class AooTest extends SolutionTest {
+
+    @Override
+    Solution getInstance() {
+        return new Aoo();
+    }
+
+}
