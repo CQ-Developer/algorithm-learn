a2c75103ef8de72fe7d23ed9a2f994dd95f92bdb
NoahChen <noah.chen@batechworks.com>
2025-02-11 10:47:36 +0800

    1695. maximum erasure value

---
+++ src/test/java/com/huhu/algorithm/learn/n1695/AooTest.java
@@ -0,0 +1,10 @@
+package com.huhu.algorithm.learn.n1695;
+
+class AooTest extends SolutionTest {
+
+    @Override
+    Solution getInstance() {
+        return new Aoo();
+    }
+
+}
---
+++ src/test/java/com/huhu/algorithm/learn/n1695/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.huhu.algorithm.learn.n1695;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+abstract class SolutionTest {
+
+    Solution alg;
+
+    abstract Solution getInstance();
+
+    @BeforeEach
+    void setup() {
+        alg = getInstance();
+    }
+
+    @Test
+    void test1() {
+        int[] nums = {4, 2, 4, 5, 6};
+        assertEquals(17, alg.maximumUniqueSubarray(nums));
+    }
+
+    @Test
+    void test2() {
+        int[] nums = {5, 2, 1, 2, 5, 2, 1, 2, 5};
+        assertEquals(8, alg.maximumUniqueSubarray(nums));
+    }
+
+}
