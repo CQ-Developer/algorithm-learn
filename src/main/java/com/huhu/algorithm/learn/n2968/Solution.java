782356c5a4d99880acf282711c7a30e2c48e5386
NoahChen <noah.chen@batechworks.com>
2025-02-28 13:35:34 +0800

    2968. apply operations to maximize frequency score

---
+++ src/main/java/com/huhu/algorithm/learn/n2968/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.n2968;
+
+/// #[apply operations to maximize frequency score](https://leetcode.cn/problems/apply-operations-to-maximize-frequency-score/description/)
+interface Solution {
+
+    int maxFrequencyScore(int[] nums, long k);
+
+}
