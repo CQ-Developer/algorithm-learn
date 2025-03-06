ba94e09e7e864b210cdce837c73c16076c095a74
NoahChen <noah.chen@batechworks.com>
2025-03-06 16:25:46 +0800

    brute force

---
+++ src/main/java/com/huhu/algorithm/learn/n395/Aoo.java
@@ -0,0 +1,28 @@
+package com.huhu.algorithm.learn.n395;
+
+/// # brute force
+class Aoo implements Solution {
+
+    @Override
+    public int longestSubstring(String s, int k) {
+        char[] c = s.toCharArray();
+        int res = 0, n = c.length;
+        for (int l = 0; l < n; l++) {
+            int unsatisfied = 0;
+            int[] counts = new int[27];
+            for (int r = l; r < n; r++) {
+                if (++counts[c[r] & 31] == 1) {
+                    unsatisfied++;
+                }
+                if (counts[c[r] & 31] == k) {
+                    unsatisfied--;
+                }
+                if (unsatisfied == 0) {
+                    res = Math.max(res, r - l + 1);
+                }
+            }
+        }
+        return res;
+    }
+
+}
