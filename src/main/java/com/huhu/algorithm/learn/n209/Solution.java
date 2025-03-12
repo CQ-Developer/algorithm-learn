80868e60f4e09f48521619ecc578ed00a68ab662
NoahChen <noah.chen@batechworks.com>
2025-03-12 09:02:03 +0800

    209. minimum size subarray sum

---
+++ src/main/java/com/huhu/algorithm/learn/n209/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.n209;
+
+/// # [minimum size subarray sum](https://leetcode.cn/problems/minimum-size-subarray-sum/description/)
+interface Solution {
+
+    int minSubArrayLen(int target, int[] nums);
+
+}
