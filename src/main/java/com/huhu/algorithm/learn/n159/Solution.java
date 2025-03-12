dc7266e1fb3aaeea953ea6bf2935731fa20e8171
NoahChen <noah.chen@batechworks.com>
2025-03-12 09:56:14 +0800

    159. longest substring with at most two distinct characters

---
+++ src/main/java/com/huhu/algorithm/learn/n159/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.n159;
+
+/// # [the longest substring with at most two distinct characters](https://leetcode.cn/problems/longest-substring-with-at-most-two-distinct-characters/description/)
+interface Solution {
+
+    int lengthOfLongestSubstringTwoDistinct(String s);
+
+}
