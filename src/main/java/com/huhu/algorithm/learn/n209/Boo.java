cdd180858f08e261c82107b72132f04029f83676
NoahChen <noah.chen@batechworks.com>
2025-03-12 09:23:48 +0800

    sliding window

---
+++ src/main/java/com/huhu/algorithm/learn/n209/Boo.java
@@ -0,0 +1,21 @@
+package com.huhu.algorithm.learn.n209;
+
+/// # sliding window
+class Boo implements Solution {
+
+    @Override
+    public int minSubArrayLen(int target, int[] nums) {
+        int mn = Integer.MAX_VALUE;
+        for (int s = 0, l = 0, r = 0; r < nums.length; r++) {
+            s += nums[r];
+            for (; s - nums[l] >= target; l++) {
+                s -= nums[l];
+            }
+            if (s >= target) {
+                mn = Math.min(mn, r - l + 1);
+            }
+        }
+        return mn == Integer.MAX_VALUE ? 0 : mn;
+    }
+
+}
