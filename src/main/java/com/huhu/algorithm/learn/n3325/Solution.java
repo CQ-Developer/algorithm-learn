77cd13c5b7d621068167a926cab868dacb486ca8
Jack <18551681083@163.com>
2025-03-18 18:51:07 +0800

    3325 count substrings with k-frequency characters i

---
+++ src/main/java/com/huhu/algorithm/learn/n3325/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.n3325;
+
+/// # [3325 count substrings with k-frequency characters i](https://leetcode.cn/problems/count-substrings-with-k-frequency-characters-i/description/)
+interface Solution {
+
+    int numberOfSubstrings(String s, int k);
+
+}
