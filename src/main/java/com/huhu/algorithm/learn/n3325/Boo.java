4be21ab93cd6fa83f301b20b066a5845a8d97c59
Jack <18551681083@163.com>
2025-03-18 23:26:35 +0800

    sliding window

---
+++ src/main/java/com/huhu/algorithm/learn/n3325/Boo.java
@@ -0,0 +1,21 @@
+package com.huhu.algorithm.learn.n3325;
+
+/// # sliding window
+class Boo implements Solution {
+
+    @Override
+    public int numberOfSubstrings(String s, int k) {
+        int[] cnt = new int[26];
+        char[] cs = s.toCharArray();
+        int n = cs.length, res = 0;
+        for (int l = 0, r = 0; r < n; r++) {
+            cnt[cs[r] - 'a']++;
+            for (; cnt[cs[r] - 'a'] >= k; l++) {
+                cnt[cs[l] - 'a']--;
+            }
+            res += l;
+        }
+        return res;
+    }
+
+}
