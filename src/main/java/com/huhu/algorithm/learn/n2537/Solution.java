a9cb169fc2a0c936a9cf13771bab337084e41cf6
Jack <18551681083@163.com>
2025-03-19 18:59:19 +0800

    2537 count the number of good subarrays

---
+++ src/main/java/com/huhu/algorithm/learn/n2537/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.n2537;
+
+/// #[2537 count the number of good subarrays](https://leetcode.cn/problems/count-the-number-of-good-subarrays/description/)
+interface Solution {
+
+    long countGood(int[] nums, int k);
+
+}
