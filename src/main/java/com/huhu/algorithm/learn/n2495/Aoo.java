de9925eb4adff2799f836840d4bd84f6a185f312
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-03-20 15:01:45 +0800

    sliding window

---
+++ src/main/java/com/huhu/algorithm/learn/n2495/Aoo.java
@@ -0,0 +1,18 @@
+package com.huhu.algorithm.learn.n2495;
+
+/// # sliding window
+class Aoo implements Solution {
+
+    @Override
+    public long evenProduct(int[] nums) {
+        long res = 0;
+        for (int l = -1, r = 0; r < nums.length; r++) {
+            if ((nums[r] & 1) == 0) {
+                l = r;
+            }
+            res += l + 1;
+        }
+        return res;
+    }
+
+}
