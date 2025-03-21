eb84711a24895870907a38632cdc77498b689d8b
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-03-21 17:07:52 +0800

    2302 count subarrays with scores less than k

---
+++ src/main/java/com/huhu/algorithm/learn/n2302/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.n2302;
+
+/// # [2302 count subarrays with scores less than k](https://leetcode.cn/problems/count-subarrays-with-score-less-than-k/description/)
+interface Solution {
+
+    long countSubarrays(int[] nums, long k);
+
+}
