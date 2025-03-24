3c68ab0fc25cf15c166a908f7f7dd3509244aea9
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-03-24 14:35:47 +0800

    lcp68 美观的花束

---
+++ src/main/java/com/huhu/algorithm/learn/lcp68/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.lcp68;
+
+/// # [lcp68 美观的花束](https://leetcode.cn/problems/1GxJYY/description/)
+interface Solution {
+
+    int beautifulBouquet(int[] flowers, int cnt);
+
+}
