5bc630d2dd6f7977b2b641e091d624e5b96e9899
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-03-24 16:57:19 +0800

    2743 count substrings without repeating character

---
+++ src/main/java/com/huhu/algorithm/learn/n2743/Solution.java
@@ -0,0 +1,8 @@
+package com.huhu.algorithm.learn.n2743;
+
+/// # [2743 count substrings without repeating character](https://leetcode.cn/problems/count-substrings-without-repeating-character/description/)
+interface Solution {
+
+    int numberOfSpecialSubstrings(String s);
+
+}
