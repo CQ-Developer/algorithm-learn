fc73e88c3b739aebb5c424dba933623eac043ff5
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-03-26 11:29:58 +0800

    3306 count of substrings containing every vowel and k consonants ii

---
+++ src/main/java/com/huhu/algorithm/learn/n3306/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n3306;
+
+/**
+ * <a href="https://leetcode.cn/problems/count-of-substrings-containing-every-vowel-and-k-consonants-ii/description/">
+ * 3306 count of substrings containing every vowel and k consonants ii
+ * </a>
+ */
+interface Solution {
+
+    long countOfSubstrings(String word, int k);
+
+}
