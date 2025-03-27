5554b99df00aa8c5bf58139fd7152c8b24d593d2
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-03-27 14:05:02 +0800

    992 subarrays with k different integers

---
+++ src/main/java/com/huhu/algorithm/learn/n992/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.n992;
+
+/**
+ * <a href="https://leetcode.cn/problems/subarrays-with-k-different-integers/description/">
+ * 992 subarrays with k different integers
+ * </a>
+ */
+interface Solution {
+
+    int subarraysWithKDistinct(int[] nums, int k);
+
+}
