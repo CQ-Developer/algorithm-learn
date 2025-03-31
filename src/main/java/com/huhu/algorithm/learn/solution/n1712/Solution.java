b2d9aff52f19f506fa071d5a082f1b17701084a4
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-03-31 11:49:25 +0800

    1712 ways to split array into three subarrays

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n1712/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n1712;
+
+/**
+ * <a href="https://leetcode.cn/problems/ways-to-split-array-into-three-subarrays/description/">
+ * 1712 ways to split array into three subarrays
+ * </a>
+ */
+interface Solution {
+
+    int waysToSplit(int[] nums);
+
+}
