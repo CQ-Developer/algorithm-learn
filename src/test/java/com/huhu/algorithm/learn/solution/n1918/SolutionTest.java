469e9dfd46720d4a7f2991e8ba266ce09b97cff1
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-01 11:15:26 +0800

    1918 kth smallest subarray sum

---
+++ src/test/java/com/huhu/algorithm/learn/solution/n1918/SolutionTest.java
@@ -0,0 +1,71 @@
+package com.huhu.algorithm.learn.solution.n1918;
+
+import com.huhu.algorithm.common.BatchSpliterator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.platform.commons.util.ClassLoaderUtils;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+abstract class SolutionTest {
+
+    Solution solution;
+
+    abstract Solution getSolution();
+
+    @BeforeEach
+    void setup() {
+        solution = getSolution();
+    }
+
+    @Test
+    void test_a() {
+        int[] nums = {2, 1, 3};
+        assertEquals(3, solution.kthSmallestSubarraySum(nums, 4));
+    }
+
+    @Test
+    void test_b() {
+        int[] nums = {3, 3, 5, 5};
+        assertEquals(10, solution.kthSmallestSubarraySum(nums, 7));
+    }
+
+    @ParameterizedTest
+    @MethodSource("generate")
+    void test_c(int[] nums, int k, int expected) {
+        assertEquals(expected, solution.kthSmallestSubarraySum(nums, k));
+    }
+
+    static Stream<Arguments> generate() throws URISyntaxException, IOException {
+        var url = ClassLoaderUtils.getDefaultClassLoader().getResource("n1918.cases");
+        if (url == null) {
+            return Stream.empty();
+        }
+        return StreamSupport
+                .stream(new BatchSpliterator<>(3, Files.lines(Paths.get(url.toURI())).spliterator()), false)
+                .flatMap(SolutionTest::generate);
+    }
+
+    static Stream<Arguments> generate(List<String> batch) {
+        if (batch.size() != 3) {
+            return Stream.empty();
+        }
+        int[] nums = Arrays.stream(batch.get(0).split(",")).mapToInt(Integer::parseInt).toArray();
+        int k = Integer.parseInt(batch.get(1));
+        int expected = Integer.parseInt(batch.get(2));
+        return Stream.of(Arguments.arguments(nums, k, expected));
+    }
+
+}
