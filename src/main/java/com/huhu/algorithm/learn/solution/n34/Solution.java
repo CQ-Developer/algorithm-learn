d02772a2e42eb6014647f039fe943efaa59b415b
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-01 13:13:43 +0800

    34 find first and last position of element in sorted array

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n34/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n34;
+
+/**
+ * <a href="https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/description/">
+ * 34 find first and last position of element in sorted array
+ * </a>
+ */
+interface Solution {
+
+    int[] searchRange(int[] nums, int target);
+
+}
