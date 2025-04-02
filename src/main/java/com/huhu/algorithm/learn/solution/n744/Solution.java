b817f9bbd25f7f87d4b7862ec48acb9b6dc113a9
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-02 11:39:52 +0800

    744 find smallest letter greater than target

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n744/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n744;
+
+/**
+ * <a href="https://leetcode.cn/problems/find-smallest-letter-greater-than-target/description/">
+ * 744 find smallest letter greater than target
+ * </a>
+ */
+interface Solution {
+
+    char nextGreatestLetter(char[] letters, char target);
+
+}
