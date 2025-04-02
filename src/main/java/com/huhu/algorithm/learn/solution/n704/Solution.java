e3567afe7bf5b41e26c3f53e354624e83271e792
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-02 10:53:12 +0800

    704 binary search

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n704/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n704;
+
+/**
+ * <a href="https://leetcode.cn/problems/binary-search/description/">
+ * 704 binary search
+ * </a>
+ */
+interface Solution {
+
+    int search(int[] nums, int target);
+
+}
