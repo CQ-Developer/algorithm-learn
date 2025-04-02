4dfab4461995307d12d2a6dcd4f80f632f9360f9
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-02 10:10:19 +0800

    35 search insert position

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n35/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n35;
+
+/**
+ * <a href="https://leetcode.cn/problems/search-insert-position/description/">
+ * 35 search insert position
+ * </a>
+ */
+interface Solution {
+
+    int searchInsert(int[] nums, int target);
+
+}
