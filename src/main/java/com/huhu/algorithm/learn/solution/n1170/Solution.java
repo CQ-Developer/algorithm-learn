5e0fee26ed0e995607354970ee0929a9c4e6afe6
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-03 13:23:33 +0800

    1170 compare strings by frequency of the smallest character

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n1170/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n1170;
+
+/**
+ * <a href="https://leetcode.cn/problems/compare-strings-by-frequency-of-the-smallest-character/description/">
+ * 1170 compare strings by frequency of the smallest character
+ * </a>
+ */
+interface Solution {
+
+    int[] numSmallerByFrequency(String[] queries, String[] words);
+
+}
