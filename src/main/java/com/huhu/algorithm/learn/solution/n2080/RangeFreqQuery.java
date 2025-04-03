125a19380fe3a9a3c1feccdb60ab6a2fee89918d
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-03 15:28:31 +0800

    2080 range frequency queries

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n2080/RangeFreqQuery.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n2080;
+
+/**
+ * <a href="https://leetcode.cn/problems/range-frequency-queries/description/">
+ * 2080 range frequency queries
+ * </a>
+ */
+interface RangeFreqQuery {
+
+    int query(int left, int right, int value);
+
+}
