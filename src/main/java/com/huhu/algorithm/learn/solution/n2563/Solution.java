e5ffb402e09a73ab90c8cb7d77850bff07a4b8bb
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-07 10:51:16 +0800

    2563 count the number of fair pairs

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n2563/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n2563;
+
+/**
+ * <a href="https://leetcode.cn/problems/count-the-number-of-fair-pairs/description/">
+ * 2563 count the number of fair pairs
+ * </a>
+ */
+interface Solution {
+
+    long countFairPairs(int[] nums, int lower, int upper);
+
+}
