57eb08b6a1272c5ac21e643b866c7182f47ab879
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-07 14:48:37 +0800

    2070 most beautiful item for each query

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n2070/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n2070;
+
+/**
+ * <a href="https://leetcode.cn/problems/most-beautiful-item-for-each-query/description/">
+ * 2070 most beautiful item for each query
+ * </a>
+ */
+interface Solution {
+
+    int[] maximumBeauty(int[][] items, int[] queries);
+
+}
