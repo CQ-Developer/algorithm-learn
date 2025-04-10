767745b11338045face5c729c33a160189cd4b76
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-10 14:58:39 +0800

    1182 shortest distance to target color

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n1182/Solution.java
@@ -0,0 +1,14 @@
+package com.huhu.algorithm.learn.solution.n1182;
+
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/shortest-distance-to-target-color/description/">
+ * 1182 shortest distance to target color
+ * </a>
+ */
+interface Solution {
+
+    List<Integer> shortestDistanceColor(int[] colors, int[][] queries);
+
+}
