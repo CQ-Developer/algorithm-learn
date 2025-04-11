6ae2772897c4b291e625d6211bc72564fbd662df
Jack <18551681083@163.com>
2025-04-11 23:57:49 +0800

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,6 @@ idea {
     }
 }
 
-repositories {
-    mavenLocal()
-    mavenCentral()
-}
-
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.12.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "algorithm-learn"
+
+dependencyResolutionManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        gradlePluginPortal()
+    }
+}
