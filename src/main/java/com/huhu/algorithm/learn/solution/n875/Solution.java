010d3bccc761012710afc78574ec8c2f50afe44c
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-15 15:05:44 +0800

    875 koko eating bananas

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n875/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n875;
+
+/**
+ * <a href="https://leetcode.cn/problems/koko-eating-bananas/description/">
+ * 875 koko eating bananas
+ * </a>
+ */
+interface Solution {
+
+    int minEatingSpeed(int[] piles, int h);
+
+}
