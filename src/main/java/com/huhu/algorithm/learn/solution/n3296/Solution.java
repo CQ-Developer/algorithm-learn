b2becb1fb42e309f69a0452a339df9704c5af510
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-15 16:20:30 +0800

    3296 minimum number of seconds to make mountain height zero

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n3296/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n3296;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-number-of-seconds-to-make-mountain-height-zero/description/">
+ * 3296 minimum number of seconds to make mountain height zero
+ * </a>
+ */
+interface Solution {
+
+    long minNumberOfSeconds(int mountainHeight, int[] workerTimes);
+
+}
