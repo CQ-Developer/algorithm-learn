2bd7db6d4b026e917a1df80a169df4fc8029f77d
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-17 13:50:00 +0800

    2604 minimum time to eat all grains

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n2604/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n2604;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-time-to-eat-all-grains/description/">
+ * 2604 minimum time to eat all grains
+ * </a>
+ */
+interface Solution {
+
+    int minimumTime(int[] hens, int[] grains);
+
+}
