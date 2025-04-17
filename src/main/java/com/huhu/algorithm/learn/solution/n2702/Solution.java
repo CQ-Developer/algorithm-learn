180633cdeace097190995887fe1befaf233f5746
Noah Chen (ext.) <noah.chen@partner.bmwgroup.com>
2025-04-17 16:41:35 +0800

    2702 minimum operations to make numbers non-positive

---
+++ src/main/java/com/huhu/algorithm/learn/solution/n2702/Solution.java
@@ -0,0 +1,12 @@
+package com.huhu.algorithm.learn.solution.n2702;
+
+/**
+ * <a href="https://leetcode.cn/problems/minimum-operations-to-make-numbers-non-positive/description/">
+ * 2702 minimum operations to make numbers non-positive
+ * </a>
+ */
+interface Solution {
+
+    int minOperations(int[] nums, int x, int y);
+
+}
